82fc116995d4ffbdc56ecda854453d44da2b8cb3
Luke Fritz <lukefritz@gmail.com>
2020-10-30 18:31:49 -0500

    Prevent zsh subshell in installer; fix permissions

--- modules/zsh/install.sh
+++ modules/zsh/install.sh
@@ -6,6 +6,11 @@ function describe_actions() {
 
 function install() {
   echo "Installing Oh My Zsh..."
+  # Prevent installer from starting a zsh subshell inside the installer
+  export RUNZSH="no"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   echo "Oh My Zsh installed."
+
+  # Restrict permissions for auto-completions
+  chmod -R 755 /usr/local/share/zsh
 }
