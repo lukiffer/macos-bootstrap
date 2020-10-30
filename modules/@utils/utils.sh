5967088f8597207c3f793a8f369b9cd16dd46d7e
Luke Fritz <lukefritz@gmail.com>
2020-10-30 18:13:54 -0500

    Remove redundant confirm prompts

--- modules/@utils/utils.sh
+++ modules/@utils/utils.sh
@@ -23,22 +23,8 @@ function confirm() {
 
 function install_homebrew_package() {
   local -r package_name="$1"
-  local -r environment_override="INSTALL_$(echo "$package_name" | /usr/bin/sed 's/-/_/' | tr '[:lower:]' '[:upper:]')"
-
-  local confirmation="false"
-
-  if [ "$MACOSBS_INSTALL_ALL" == "true" ]; then
-    confirmation="true"
-  else
-    confirmation=$(confirm "$environment_override" "Do you want to install $package_name?")
-  fi
-
-  if [ "$confirmation" == "true" ]; then
-    echo "Installing homebrew package $package_name..."
-    brew install "$package_name"
-  else
-    echo "Homebrew package $package_name will not be installed."
-  fi
+  echo "Installing homebrew package $package_name..."
+  brew install "$package_name"
 }
 
 function get_dropbox_mount_path() {
