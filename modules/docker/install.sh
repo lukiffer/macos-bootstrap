795ddb030cc7cff173f68db2e39a7e83bea0b63a
Luke Fritz <lukefritz@gmail.com>
2023-06-15 17:40:12 -0500

    Remove xhyve from docker module (patch)

--- modules/docker/install.sh
+++ modules/docker/install.sh
@@ -4,14 +4,10 @@ function describe_actions() {
   echo "   📦  Install the latest docker package from Homebrew"
   echo "   📦  Install the latest docker-compose package from Homebrew"
   echo "   📦  Install the latest docker-machine package from Homebrew"
-  echo "   📦  Install the latest xhyve package from Homebrew"
-  echo "   📦  Install the latest docker-machine-driver-xhyve package from Homebrew"
 }
 
 function install() {
   install_homebrew_package "docker"
   install_homebrew_package "docker-compose"
   install_homebrew_package "docker-machine"
-  install_homebrew_package "xhyve"
-  install_homebrew_package "docker-machine-driver-xhyve"
 }
